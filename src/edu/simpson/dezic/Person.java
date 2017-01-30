c05bb98522c907ff10464b4b0f759738b2f74ff5
RASIM\Rasim Dezic <rasim.dezic@my.simpson.edu>
2017-01-30 13:40:38 -0600

    Updated versions of previous files that allows for the web server to print out the information on the MySQL in Json format.

---
+++ src/edu/simpson/dezic/Person.java
@@ -0,0 +1,31 @@
+package edu.simpson.dezic;
+
+public class Person {
+
+    private int id;
+    private String first;
+    private String last;
+
+    private String email;
+    private String phone;
+    private String birthday;
+
+    public int getId() {return id; }
+    public void setId(int id) { this.id = id; }
+
+    public String getFirst() {return first; }
+    public void setFirst(String first) {this.first = first; }
+
+    public String getLast() {return last; }
+    public void setLast(String last) {this.last = last; }
+
+    public String getEmail() {return email; }
+    public void setEmail(String email) {this.email = email; }
+
+    public String getPhone() {return phone; }
+    public void setPhone(String phone) {this.phone = phone; }
+
+    public String getBirthday() {return birthday; }
+    public void setBirthday(String birthday) {this.birthday = birthday; }
+
+}
